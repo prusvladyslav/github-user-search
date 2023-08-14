4182e91c279f87708471aa86df6e67cf65b3585d
vlpr <79951438+prusvladyslav@users.noreply.github.com>
2023-08-14 15:11:11 +0300

    small improvment & updated readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 npm i > npm start 
+
+Branches: 
+main > redux version
+react-query > react-query version (I like it better)
--- src/components/GitHubSearch/GitHubSearch.tsx
+++ src/components/GitHubSearch/GitHubSearch.tsx
@@ -4,18 +4,18 @@ import { AppDispatch, RootState } from "../../redux/store";
 import { clearState, getUser } from "../../redux/userSlice";
 
 export const GitHubSearch = ({ }) => {
-    const [input, setInput] = useState('prusvladyslav');
+    const [username, setUsername] = useState('prusvladyslav');
     const dispatch: AppDispatch = useDispatch();
     const searchUser = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault()
         dispatch(clearState());
-        dispatch(getUser(input));
+        dispatch(getUser(username));
     };
     const loading = useSelector((state: RootState) => state.user.loading);
 
     return (
         <form className="search-container" onSubmit={searchUser}>
-            <input value={input} onChange={(e) => setInput(e.target.value)} />
+            <input value={username} onChange={(e) => setUsername(e.target.value)} />
             <button type="submit" disabled={loading === 'pending'}>Search</button>
         </form>
     );
